779cb681a2b0ee549a2f20e7cf58c5bb67fa7749
Евгений Попов <popov.eugene@alanov.ru>
2014-09-18 14:43:20 +0700

    Добавление базовых стилей и подключение загрузки шрифтов

--- assets/js/core.js
+++ assets/js/core.js
@@ -1,3 +1,15 @@
 /**
  * Created by kaaboeld on 18.09.14.
  */
+var WebFontConfig = {
+	google: { families: [ 'Roboto:400,400italic,300,300italic,700,700italic:latin' ] }
+};
+(function() {
+	var wf = document.createElement('script');
+	wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+		'://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+	wf.type = 'text/javascript';
+	wf.async = 'true';
+	var s = document.getElementsByTagName('script')[0];
+	s.parentNode.insertBefore(wf, s);
+})();
